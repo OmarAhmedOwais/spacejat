de2f0e55593fed50c76ce35e332a8ee8e7f1d690
OmarAhmed0001 <oa1476@fayoum.edu.eg>
2024-05-15 02:18:49 +0300

    Co-authored-by: Karim Ahmed Mahmoud Ahmed <ka1396@fayoum.edu.eg>

--- server.ts
+++ server.ts
@@ -1,59 +1,64 @@
+import 'colors';
+import dotenv from 'dotenv';
+
 import fastify, { FastifyInstance } from 'fastify';
 import swagger from 'fastify-swagger';
+import cors from 'cors';
 import { Server, IncomingMessage, ServerResponse } from 'http';
 import WebSocket from 'ws';
 
-import { initSocket } from 'config/io_connection';
+import db_connection from 'config/db_connection';
+import { initSocket } from 'config/ws_connection';
 import userRoutes from './src/routes/user.router';
-import db_connection from 'config/db_connection'; // import the db_connection function
-import 'colors'
-import dotenv from 'dotenv'
-dotenv.config()
+import { globalErrorMiddleware } from '@/middlewares';
+// import the db_connection function
+dotenv.config();
 
 const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify();
 app.register(swagger, {
-    routePrefix: '/documentation',
-    swagger: {
-        info: {
-            title: 'Fastify API',
-            description: 'Documentation for Fastify API',
-            version: '1.0.0'
-        },
-        externalDocs: {
-            url: 'https://swagger.io',
-            description: 'Find more info here'
-        },
-        consumes: ['application/json'],
-        produces: ['application/json']
+  routePrefix: '/documentation',
+  swagger: {
+    info: {
+      title: 'Fastify API',
+      description: 'Documentation for Fastify API',
+      version: '1.0.0',
     },
-    exposeRoute: true
+    externalDocs: {
+      url: 'https://swagger.io',
+      description: 'Find more info here',
+    },
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  },
+  exposeRoute: true,
 });
 
 app.register(userRoutes, { prefix: '/api' });
+app.register(globalErrorMiddleware);
 
-let wss :WebSocket.Server ; // define wss outside the function
+let wss: WebSocket.Server; // define wss outside the function
 
 export const start = async (): Promise<void> => {
-    try {
-        db_connection(); // connect to MongoDB
+  try {
+    db_connection(); // connect to MongoDB
 
-        wss = initSocket(app.server);
+    wss = initSocket(app.server);
 
-        wss.on('connection', (ws) => {
-            console.log('WebSocket client connected'.green);
-            
-            ws.on('message', (message) => {
-                console.log('Received message:'.blue, message);
-                // Handle message and send response back to client
-                ws.send('Message received by server'.blue);
-            });
-        });
+    wss.on('connection', (ws) => {
+      console.log('WebSocket client connected'.green);
 
-        await app.listen(3000);
-        console.log(`Server listening on http://localhost:3000`.green);
-    } catch (error) {
-        console.error('Error starting server:'.red, error);
-    }
+      ws.on('message', (message) => {
+        console.log('Received message:'.blue, message);
+        // Handle message and send response back to client
+        ws.send('Message received by server'.blue);
+      });
+    });
+
+    await app.listen(3000);
+    console.log(`Server listening on http://localhost:3000`.green);
+  } catch (error) {
+    console.error('Error starting server:'.red, error);
+  }
 };
 
 start();
--- src/middlewares/globalError.middleware.ts
+++ src/middlewares/globalError.middleware.ts
@@ -1,44 +1,48 @@
-import { NextFunction, Request, Response } from 'express';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { StatusCodes } from 'http-status-codes';
 
 import { ApiError } from '@/util';
 import { Status } from '@/types/enums/status.enum';
 
-export const globalErrorMiddleware = async (
-  err: ApiError,
-  req: Request,
-  res: Response,
-  _next: NextFunction,
-) => {
-  const isPredicted = err?.statusCode && err?.msg;
-  const envExtra =
-    process.env.NODE_ENV === 'prod'
-      ? {
-          prod: {
-            name: err.name,
-            message: err.message,
-          },
-        }
-      : {
-          dev: {
-            name: err.name,
-            message: err.message,
-            stack: err.stack,
-          },
-        };
-  if (!isPredicted) {
-    console.log('err');
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      status: Status.ERROR,
-      error_en: err.message,
-      error_ar: err.message,
-      ...envExtra,
-    });
-  }
-  res.status(err.statusCode).json({
-    status: Status.ERROR,
-    error_en: err.msg.en,
-    error_ar: err.msg.ar,
-    ...envExtra,
-  });
+export const globalErrorMiddleware = (app: FastifyInstance) => {
+  app.setErrorHandler(
+    async (
+      err: ApiError,
+      _req: FastifyRequest,
+      reply: FastifyReply,
+    ) => {
+      const isPredicted = err?.statusCode && err?.msg;
+      const envExtra =
+        process.env.NODE_ENV === 'prod'
+          ? {
+              prod: {
+                name: err.name,
+                message: err.message,
+              },
+            }
+          : {
+              dev: {
+                name: err.name,
+                message: err.message,
+                stack: err.stack,
+              },
+            };
+      if (!isPredicted) {
+        console.log('err');
+        reply.code(StatusCodes.INTERNAL_SERVER_ERROR).send({
+          status: Status.ERROR,
+          error_en: err.message,
+          error_ar: err.message,
+          ...envExtra,
+        });
+      } else {
+        reply.code(err.statusCode).send({
+          status: Status.ERROR,
+          error_en: err.msg.en,
+          error_ar: err.msg.ar,
+          ...envExtra,
+        });
+      }
+    },
+  );
 };
